8c734c7b7de9d1151356c77e5c1f0e4b26697b6f
paramanandnarsinhabhat <141020967+paramanandnarsinhabhat@users.noreply.github.com>
2024-01-01 15:01:53 +0530

    load data

--- scripts/time_series_moving_average.py
+++ scripts/time_series_moving_average.py
@@ -9,3 +9,8 @@ from statistics import mean
 
 import warnings
 warnings.filterwarnings("ignore")
+
+
+train_data = pd.read_csv("/Users/paramanandbhat/Downloads/4.3_and_4.4_-_MA_and_WMA/data/train_data.csv")
+valid_data = pd.read_csv("/Users/paramanandbhat/Downloads/4.3_and_4.4_-_MA_and_WMA/data/valid_data.csv")
+
